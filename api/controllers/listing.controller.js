eb09660c6f88f58867d29ca3d2d4f4b003450ef1
Akshay-R-7 <akshay45549@gmail.com>
2024-07-28 10:35:44 +0530

    update listing api and page funct

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,58 @@
 import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js"
 
 export const createListing =async (req, res, next)=>{
     try{
         const listing = await Listing.create(req.body)
         return res.status(201).json(listing)
+    } catch(err){
+        next(err)
+    }
+}
+
+export const deleteListing = async (req, res, next)=>{
+    const listing = await Listing.findById(req.params.id)
+    if(!listing){
+        return next(errorHandler(404,"Listing dosen't exist"))
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,"You can only delete your own Listings!"))
+    }
+    try{
+        await Listing.findByIdAndDelete(req.params.id)
+        res.status(200).json("Listig has been deleted")
+    }catch(err){
+        next(err)
+    }
+}
+
+export const updateListing =async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id)
+    if(!listing){
+        return next(errorHandler(404,"Listing dosen't exist"))
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,"You can only update your own Listings"))
+    } 
+    try{
+        const updatedListings =await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new : true}
+        );
+        res.status(200).json(updatedListings)
+    } catch(err){
+        next(err)
+    }
+}
+
+export const getListing =async (req, res, next) => {
+    try{
+        const listing = await Listing.findById(req.params.id);
+        if(!listing){
+            return next(errorHandler(404,"Listing not found"))
+        }
+        res.status(200).json(listing)
     } catch(err){
         next(err)
     }
