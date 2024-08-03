0a4f602b9e04bce1d45ce8b056ff251a05c305a4
Akshay-R-7 <akshay45549@gmail.com>
2024-08-03 21:55:49 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Akshay Estate</h1>
+      <p className="mb-4 text-slate-700 ">
+        Welcome to Akshay Estate, your premier destination for modern,
+        luxurious, and affordable living spaces. Established with a vision to
+        redefine urban living, Akshay Estate offers a diverse portfolio of
+        properties, from elegant apartments to sprawling family homes, designed
+        to meet the needs and desires of contemporary residents. Our commitment
+        to quality, innovation, and customer satisfaction ensures that every
+        Akshay Estate property is a testament to our dedication to excellence.
+        Whether you're looking for a vibrant community in the heart of the city
+        or a tranquil retreat in the suburbs, Akshay Estate has the perfect home
+        for you.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Akshay Estate, we believe that a home is more than just a place to
+        live; it's a reflection of your lifestyle and aspirations. Our
+        properties feature state-of-the-art amenities, sustainable design
+        practices, and meticulously crafted interiors that offer comfort,
+        convenience, and a touch of luxury. We work with renowned architects and
+        designers to create spaces that are not only aesthetically pleasing but
+        also highly functional. From energy-efficient appliances to smart home
+        technology, every detail is thoughtfully considered to enhance your
+        living experience.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our dedication to customer service sets Akshay Estate apart in the real
+        estate market. We understand that buying a home is one of the most
+        significant investments you will make, and our team is here to support
+        you every step of the way. From the initial consultation to the final
+        walkthrough, we strive to provide a seamless and enjoyable experience.
+        At Akshay Estate, we are proud to help you find not just a house, but a
+        place you can truly call home. Join our growing community and discover
+        the perfect blend of luxury, comfort, and modern living with Akshay
+        Estate.
+      </p>
+    </div>
+  );
 }
-
